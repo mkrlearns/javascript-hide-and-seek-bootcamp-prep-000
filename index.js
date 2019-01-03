50d4ca169a89ab13999f320d3b44246487ac029a
ryan m <contact@ryanmeek.com>
2019-01-03 20:02:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function getFirstSelector(selector) {
   return document.querySelectorAll(selector)[0]
 }
+let nested = document.getElementById('nested')
+function nestedTarget() {
+  function find(element) {
+    nested = nested.querySelectorAll(*)
+    for (let i = 0; nested.length > i; i++) {
+      
+    }
+  }
+}

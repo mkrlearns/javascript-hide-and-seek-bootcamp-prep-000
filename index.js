ca298fc4e3b950d65fd7b85be8866439312f3e9e
ryan m <contact@ryanmeek.com>
2019-01-03 20:15:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 function getFirstSelector(selector) {
   return document.querySelectorAll(selector)[0]
 }
-let nested = document.getElementById('app')
-nested = document.getElementById('nested')
+
 function nestedTarget() {
-  var done = nested.getElementByClassName('target');
-  if (done) return done;
-  node = nested.firstChild;
-  while (!done) {
-    done = nestedTarget();
-    if (done) return done;
-    node = nested.nextSibling;
-  }
+
 }

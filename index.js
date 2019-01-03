3eb2f4ac920faf2bcd3700d1df2f97f4357f02bc
ryan m <contact@ryanmeek.com>
2019-01-03 21:29:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const ranked_list = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < ranked_list.length; i++){
+    ranked_list[i].innerHTML = parseInt((i + n))
+  }
+}
+
+function deepestChild( ){
+   var parent = document.getElementById("grand-node")
+   var child = parent.children[0]
+   while (child) {
+     parent = child
+     child = parent.children[0]
+   }
+   return parent
+}
